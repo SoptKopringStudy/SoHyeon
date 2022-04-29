cb29f36b81a49b9cb671db721f498953f903d9ba
thguss <thgus345@naver.com>
2022-04-29 15:40:27 +0900

    1차 과제

---
+++ study1/src/main/kotlin/com/example/kopring/KopringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringApplication
+
+fun main() {
+    runApplication<SpringBootApplication>()
+}
