035ebe185ed123de57c08026bdb81ce13bd2b34f
thguss <thgus345@naver.com>
2022-05-11 21:16:58 +0900

    init commit

---
+++ src/main/kotlin/com/sohyeon/kopring/assignment1/Week1Controller.kt
@@ -0,0 +1,10 @@
+package com.sohyeon.kopring.assignment1
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/user")
+@RestController
+class Week1Controller {
+
+}
